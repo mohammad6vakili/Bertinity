c6844be37b8e35a61322d1eea0ccf15cfb7dcb42
mohammad6vakili <59134745+mohammad6vakili@users.noreply.github.com>
2021-08-28 21:26:52 +0430

    hamburger menu and footer

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var hello=document.getElementById("hello");
-hello.addEventListener("click",func);
-
-function func(){
-    alert("hello")
+//---------------------------------------------------------Hamburger Menu------------------------------------
+const hamMenu=document.getElementById('hamburger-menu');
+document.getElementById('hamburger-button').onclick=()=>{
+    hamMenu.classList.add('hamburger-open');
+    hamMenu.style.display='flex';
+};
+document.getElementById('close-ham').onclick=()=>{
+    hamMenu.style.display='none';
+    hamMenu.classList.remove('hamburger-open');
 }
