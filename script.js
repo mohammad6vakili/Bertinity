bc3d2bc815fe94d8d5d339b0c463d3d269297627
mohammad6vakili <59134745+mohammad6vakili@users.noreply.github.com>
2021-09-04 22:29:11 +0430

    swiper slider and complete pages

--- script.js
+++ script.js
@@ -10,5 +10,32 @@ document.getElementById('close-ham').onclick=()=>{
 }
 //------------------------------------------carousel----------------------------------------
 $('.carousel').carousel({
-    interval: 60000000
+    interval: 6000
   })
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: "fade",
+    loop: true,
+    speed: 1200,
+    autoplay: {
+      delay: 6000000,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
